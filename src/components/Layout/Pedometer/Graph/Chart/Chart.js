6e28be8da25baecd03a700ab919251e215471915
Kirill <kirpetrosian@mail.ru>
2020-04-14 19:40:19 +0500

    added some styling to chart

--- src/components/Layout/Pedometer/Graph/Chart/Chart.js
+++ src/components/Layout/Pedometer/Graph/Chart/Chart.js
@@ -22,13 +22,20 @@ export default function ({ activities }) {
       width={809}
       height={430}
       data={data}
-      margin={{ right: 20, left: 20, top:10, bottom:10 }}
+      margin={{ right: 20, left: 20, top: 10, bottom: 10 }}
     >
-      <CartesianGrid style={{border:'1px solid rgba(28, 32, 37, 0.1)'}} />
-      <XAxis dataKey="name" padding={{ left: 50, right: 30 }}/>
-      <YAxis />
+      <CartesianGrid style={{ border: "1px solid rgba(28, 32, 37, 0.1)" }} />
+      <XAxis dataKey="name" padding={{ left: 50, right: 30 }} tickLine={false} interval="preserveStart"/>
+      <YAxis axisLine={false} tickLine={false}/>
       <Tooltip />
-      <Line type="monotone" dataKey="value" stroke="#EC174F" />
+      <Line
+        type="monotone"
+        dataKey="value"
+        stroke="#EC174F"
+        strokeWidth="2"
+        dot={{ stroke: "#EC174F", fill: "#EC174F", r:5 }}
+        activeDot={{r:10, stroke: "none"}}
+      />
     </LineChart>
   );
 }
